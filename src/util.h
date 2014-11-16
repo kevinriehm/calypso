988861ef8312af04e40ae493ded5a48c63fe5af2
Kevin Riehm <kevin.riehm@gmail.com>
2014-11-15 21:31:08 -0500

    Added configurable message output level

--- src/util.h
+++ src/util.h
@@ -1,9 +1,27 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#ifndef MESSAGE_LEVEL
+#define MESSAGE_LEVEL 0
+#endif
+
+#if MESSAGE_LEVEL >= 0
 #define error(...) message("error",__VA_ARGS__)
+#else
+#define error(...)
+#endif
+
+#if MESSAGE_LEVEL >= 1
 #define info(...)  message("info", __VA_ARGS__)
+#else
+#define info(...)
+#endif
+
+#if MESSAGE_LEVEL >= 2
 #define debug(...) message("debug",__VA_ARGS__)
+#else
+#define debug(...)
+#endif
 
 void message(char *, char *, ...);
 void die(char *, ...);
